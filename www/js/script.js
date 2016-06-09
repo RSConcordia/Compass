f48b5576c5d1a5ebd6785e3c7a19996a4e1722b5
RSConcordia <renansconcordia@gmail.com>
2016-06-09 11:39:39 -0300

    Sample

---
+++ www/js/script.js
@@ -0,0 +1,28 @@
+﻿
+	var ball   = document.querySelector('.ball');
+	var garden = document.querySelector('.garden');
+	var output = document.querySelector('.output');
+
+	var maxX = garden.clientWidth  - ball.clientWidth;
+	var maxY = garden.clientHeight - ball.clientHeight;
+
+	function handleOrientation(event) {
+		var x = event.beta;  // In degree in the range [-180,180]
+		var y = event.gamma; // In degree in the range [-90,90]
+
+		output.innerHTML  = "</i style='color: #70010a;'>β</i>: " + x + "\n";
+		output.innerHTML += "</i style='color: #70010a;'>γ</i>: " + y + "\n";
+
+		if (x >  90) { x =  90};
+		if (x < -90) { x = -90};
+
+		x += 90;
+		y += 90;
+
+		ball.style.top  = (maxX*x/180 - 10) + "px";
+		ball.style.left = (maxY*y/180 - 10) + "px";
+	}
+
+	function start(){
+		window.addEventListener('deviceorientation', handleOrientation);
+	}
