91b4ec3a3a77007a4b85a52718a5299df25455b7
RSConcordia <renansconcordia@gmail.com>
2016-06-24 15:22:08 -0300

    Sample

--- www/js/index.js
+++ www/js/index.js
@@ -19,19 +19,20 @@
 		var watchID = navigator.compass.watchHeading(onSuccess, onError, {frequency: 500});
 	}, false);
 	 */
+	for(i=0;i < 60;i++){
+		var set = ((i%2) == 0) ? '-' : ' ';
+		document.getElementById('circle').innerHTML += '<li style=" transform: rotateZ('+i*6+'deg) translateX(90px); -webkit-transform: rotateZ('+i*6+'deg) translateX(90px); position: absolute;transition: all .25s;width: 20px; height: 20px;transition: all .5s;color: #a1a1a1;transition: all .25s linear .25s;">'+set+'</li>';
+	}
 	try{
-		document.addEventListener("deviceready", function(){
-			var compass = navigator.compass.watchHeading(
-				function onSuccess(heading){
+		window.addEventListener("deviceorientation",
+			function onSuccess(heading)
+			{
 					document.getElementById('needle').style.transform = 'rotateZ('+parseInt(heading.magneticHeading)+'deg) translateX(0px)';
 					document.getElementById('needle').style.webkitTransform  = 'rotateZ('+parseInt(heading.magneticHeading)+'deg) translateX(0px)';
 					document.getElementById('circle').style.transform = 'rotateZ('+parseInt(heading.magneticHeading)+'deg) translateX(0px)';
 					document.getElementById('circle').style.webkitTransform  = 'rotateZ('+parseInt(heading.magneticHeading)+'deg) translateX(0px)';
-				},
-				function error(e){
-					alert('CompassError: '+e.code);
-				});
-		}, false);
+			},
+		true);
 	}
 	catch(e){
 		alert('TryCatch' +e.message);
