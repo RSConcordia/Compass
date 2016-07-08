5fbb11fe6bb0b4235c8c44a04ac3575557076120
RSConcordia <renansconcordia@gmail.com>
2016-07-08 11:58:07 -0300

    Sample

--- www/js/index.js
+++ www/js/index.js
@@ -1,27 +1,10 @@
-/* 	
-	function onSuccess(heading){
-		document.getElementById('needle').style.transform = 'rotateZ('+parseInt(heading.magneticHeading)+'deg) translateX(0px)';
-		document.getElementById('needle').style.webkitTransform  = 'rotateZ('+parseInt(heading.magneticHeading)+'deg) translateX(0px)';
-		document.getElementById('circle').style.transform = 'rotateZ('+parseInt(heading.magneticHeading)+'deg) translateX(0px)';
-		document.getElementById('circle').style.webkitTransform  = 'rotateZ('+parseInt(heading.magneticHeading)+'deg) translateX(0px)';
-	};
-	
-	function onError(error){
-		 alert('CompassError: ' + error.code);
-	};
-	
-	document.addEventListener("deviceready", function(){
-		var watchID = navigator.compass.watchHeading(onSuccess, onError, {frequency: 500});
-	}, false);
-	 */
+
 	 
-	document.getElementById('circle').innerHTML += '<li style=" transform: rotateZ(270deg) translateX(90px); -webkit-transform: rotateZ(270deg) translateX(125px); position: absolute;transition: all .25s;width: 20px; height: 20px;transition: all .5s;color: #F00;transition: all .25s linear .25s;">►</li>';
+	document.getElementById('circle').innerHTML += '<li style=" right:85%; transform: rotateZ(270deg) translateX(125px); -webkit-transform: rotateZ(270deg) translateX(125px); position: absolute;transition: all .25s;width: 20px; height: 20px;transition: all .5s; font-size:42px;color: #F00;transition: all .25s linear .25s;">➢</li>';
 	 
-	for(i=0;i < 60;i++){
-	
+	for(i=0;i < 60;i++){	
 		var set = ((i%2) == 0) ? '-' : ' ';
-		document.getElementById('circle').innerHTML += '<li style=" transform: rotateZ('+i*6+'deg) translateX(90px); -webkit-transform: rotateZ('+i*6+'deg) translateX(95px); position: absolute;transition: all .25s;width: 20px; height: 20px;transition: all .5s;color: #a1a1a1;transition: all .25s linear .25s;">'+set+'</li>';
-		
+		document.getElementById('circle').innerHTML += '<li style=" transform: rotateZ('+i*6+'deg) translateX(90px); -webkit-transform: rotateZ('+i*6+'deg) translateX(105px); position: absolute;transition: all .25s;width: 20px; height: 20px;transition: all .5s;color: #a1a1a1;transition: all .25s linear .25s;">'+set+'</li>';
 	}
 	
  	document.addEventListener("deviceready", function(){
