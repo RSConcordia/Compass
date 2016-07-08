50f63c45bffe858827f4919b23a6858ba1253796
RSConcordia <renansconcordia@gmail.com>
2016-07-08 11:45:09 -0300

    Sample

--- www/js/index.js
+++ www/js/index.js
@@ -27,11 +27,9 @@
  	document.addEventListener("deviceready", function(){
 		var compass = navigator.compass.watchHeading(
 			function onSuccess(heading){
-				var a = parseInt(heading.magneticHeading) - 360;
+				var a = -parseInt(heading.magneticHeading) ;
 				document.getElementById('rosadosventos').style.transform = 'rotateZ('+a+'deg) translateX(0px)';
 				document.getElementById('rosadosventos').style.webkitTransform  = 'rotateZ('+a+'deg) translateX(0px)';
-				document.getElementById('circle').style.transform = 'rotateZ('+(a-180)+'deg) translateX(0px)';
-				document.getElementById('circle').style.webkitTransform  = 'rotateZ('+(a-180)+'deg) translateX(0px)';
 			},
 			function error(e){
 				document.getElementById('status').innerHTML = e.message; 
