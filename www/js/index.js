ca616d7fe945259cbe76f2b6d04e4ae1dbeaa216
RSConcordia <renansconcordia@gmail.com>
2016-07-12 08:44:33 -0300

    Sample

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,6 @@
 
-	 
 	document.getElementById('circle').innerHTML += '<li style=" right:85%; transform: rotateZ(270deg) translateX(125px); -webkit-transform: rotateZ(270deg) translateX(125px); position: absolute;transition: all .25s;width: 20px; height: 20px;transition: all .5s; font-size:42px;color: #F00;transition: all .25s linear .25s;">➢</li>';
-	 
+	
 	for(i=0;i < 60;i++){	
 		var set = ((i%2) == 0) ? '-' : ' ';
 		document.getElementById('circle').innerHTML += '<li style=" transform: rotateZ('+i*6+'deg) translateX(90px); -webkit-transform: rotateZ('+i*6+'deg) translateX(105px); position: absolute;transition: all .25s;width: 20px; height: 20px;transition: all .5s;color: #a1a1a1;transition: all .25s linear .25s;">'+set+'</li>';
@@ -10,9 +9,9 @@
  	document.addEventListener("deviceready", function(){
 		var compass = navigator.compass.watchHeading(
 			function onSuccess(heading){
-				var a = parseInt(heading.magneticHeading) ;
-				document.getElementById('rosadosventos').style.transform = 'rotateZ(-'+a+'deg) translateX(0px)';
-				document.getElementById('rosadosventos').style.webkitTransform  = 'rotateZ(-'+a+'deg) translateX(0px)';
+				var a = Math.round(heading.magneticHeading) ;
+				document.getElementById('rosadosventos').style.transform = 'rotateZ('+a+'deg) translateX(0px)';
+				document.getElementById('rosadosventos').style.webkitTransform  = 'rotateZ('+a+'deg) translateX(0px)';
 			},
 			function error(e){
 				document.getElementById('status').innerHTML = e.message; 
